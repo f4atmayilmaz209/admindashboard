9d84df01cb323df290ba979f231a820c18e7f7e6
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-08-29 17:49:12 +0300

    React,Nextjs,Responsive Coding,recharts,React-hook-form,Typescript,Tailwindcss

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"images.pexels.com"
-            },
-            {
-                protocol:"https",
-                hostname:"unsplash.com"
-            }
-        ]
+        remotePatterns:[{hostname:"images.pexels.com"}]
     }
 };
 
